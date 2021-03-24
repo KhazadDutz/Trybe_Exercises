f5a94aee63547dba3957b06b08fb3422f637cbf4
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-03-24 19:13:31 -0300

    exercicio 5 completo

--- exercises/bloco4/dia1/Exercicios.js
+++ exercises/bloco4/dia1/Exercicios.js
@@ -46,3 +46,15 @@ if (a === 0) {
   console.log ('negative')
 }
 
+//Exercicio 5
+let x = 1;
+let y = 89;
+let z = 90;
+
+if (x <= 0 || y <= 0 || z <= 0){
+  console.log (false)
+}else if (x + y + z === 180){
+  console.log (true)
+}else{
+  console.log (false)
+}
