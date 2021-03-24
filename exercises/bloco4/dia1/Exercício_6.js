b4784e5b7ed934c529fee97adf01b5f0172bb20e
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-03-24 20:47:53 -0300

    exercicio 7 completo

--- exercises/bloco4/dia1/Exercício_6.js
+++ exercises/bloco4/dia1/Exercício_6.js
@@ -1,3 +1,9 @@
-var pecaXadres = prompt('Nomeie uma peça de Xadres');
-if pecaXadres.toLowerCase() =='[torre, cavalo, bispo, rei, rainha, peão]' {
+let pecaXadres = 'Torre';
+let res = pecaXadres.toLowerCase('torre')
+if (res === 'torre') {
+  console.log ('L')
+}else {
+  console.log ('Insira um nome válido')
 }
+
+//criar um programa para aceitar qualquer peça de xadres
---
+++ exercises/bloco4/dia1/Exercício_7.js
@@ -0,0 +1,16 @@
+let Porcentagem = 1;
+if (Porcentagem < 0 || Porcentagem > 100) {
+  console.log('Insira um número entre 0 - 100')
+}else if (Porcentagem >= 90) {
+  console.log('A');
+}else if (Porcentagem >= 80) {
+  console.log('B');
+}else if (Porcentagem >= 70) {
+  console.log('C');
+}else if (Porcentagem >= 60) {
+  console.log('D');
+}else if (Porcentagem >= 50) {
+  console.log('E');
+}else if (Porcentagem < 50) {
+  console.log('F');
+}
