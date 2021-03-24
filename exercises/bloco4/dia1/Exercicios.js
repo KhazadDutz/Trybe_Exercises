69a53dad423698ab2fd8e25c58d1057de6f6f51e
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-03-24 18:29:29 -0300

    exercicio 4 completo

--- exercises/bloco4/dia1/Exercicios.js
+++ exercises/bloco4/dia1/Exercicios.js
@@ -35,5 +35,14 @@ if (a > b && a > c) {
   console.log (c)
 }
 
+//Exercicio 4
+let a = 42;
 
+if (a === 0) {
+  console.log ('zero');
+}else if (a > 0) {
+  console.log ('positive')
+}else{
+  console.log ('negative')
+}
 
