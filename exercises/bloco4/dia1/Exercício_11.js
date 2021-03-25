59b4836cd2ec3812b1ae2507a118e611df4fcd98
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-03-25 12:19:28 -0300

    inicia o exercicio 11

---
+++ exercises/bloco4/dia1/Exercício_11.js
@@ -0,0 +1,13 @@
+let salarioBruto = 3000;
+
+if (salarioBruto <= 1556.94) {
+  console.log(salarioBruto - (8/100 * salarioBruto));  
+}else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+  console.log(salarioBruto - (9/100 * salarioBruto));
+}else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+  console.log(salarioBruto - (11/100 * salarioBruto));
+}else{
+  console.log(salarioBruto - 570.88);
+}
+
+let salarioBase = 
