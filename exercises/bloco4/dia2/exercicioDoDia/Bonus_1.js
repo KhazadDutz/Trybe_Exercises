3b843163c92a8a36de52b0657c116e7ecedad250
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-03-26 13:54:20 -0300

    comeca os exercicios bonus

---
+++ exercises/bloco4/dia2/exercicioDoDia/Bonus_1.js
@@ -0,0 +1,10 @@
+let numbers = [5, 3, 2, 4, 7, 1, 0, 6]
+
+for (indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
+  for (indexDupla = 1; indexDupla < numbers.length - indexNumbers; indexDupla +=1) {
+    if (indexDupla > indexNumbers) {
+      let posicao = numbers[indexNumbers]
+      
+    }
+  }
+}
