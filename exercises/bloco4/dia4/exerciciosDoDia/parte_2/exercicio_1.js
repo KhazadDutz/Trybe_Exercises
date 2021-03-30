94b71e8537475b57ba4b6b5524f3a6bac7baedb6
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-03-30 13:40:39 -0300

    exercicio 1 completo

--- exercises/bloco4/dia4/exerciciosDoDia/parte_2/exercicio_1.js
+++ exercises/bloco4/dia4/exerciciosDoDia/parte_2/exercicio_1.js
@@ -1,15 +1,14 @@
 function reverse(s) {
-  let palavraNormal = s
-  let palavraInvertida = '';
-  for (let i = s.length - 1; i >= 0; i -= 1) {
-      palavraInvertida += s[i];
+  var o = '';
+  for (var i = s.length - 1; i >= 0; i--) {
+      o += s[i];
   }
-  return palavraInvertida;
+  if (o === s){
+    return(true);
+  }else{
+    return(false);
+  }
 }
 
-if (palavraInvertida === palavraNormal) {
-  console.log(true);
-}else{
-  console.log(false);
-}
-  
+let palavra = 'arara';
+console.log(reverse (palavra));
