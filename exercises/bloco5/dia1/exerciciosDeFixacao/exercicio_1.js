4c18974da8e099309ad4142fc6d9eec0ca7e811a
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-05 15:09:15 -0300

    adiciona o arquivo .js

---
+++ exercises/bloco5/dia1/exerciciosDeFixacao/exercicio_1.js
@@ -0,0 +1,6 @@
+document.getElementById('page-title').innerText = 'How to Train Your Dragon';
+let paragraph = document.getElementById('paragraph');
+paragraph.style.color = 'rgb(158, 123, 246)';
+document.getElementById('subtitle').innerText = 'Dragons are AWESOME!'
+let subtitle = document.getElementById('subtitle');
+subtitle.style.color = 'rgb(0, 204, 163)';
