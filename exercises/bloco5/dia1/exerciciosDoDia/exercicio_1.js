0014c760b960e59e8c4b8750ed3cf882394e91fd
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-05 18:38:15 -0300

    exercicio 1 completo

---
+++ exercises/bloco5/dia1/exerciciosDoDia/exercicio_1.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName('p')[0].innerText = 'me vejo sendo um programador junior';
+document.getElementsByTagName('p')[1].innerText = 'trabalhando na área de backend';
+document.getElementsByTagName('p')[2].innerText = 'preferencialmente em alguma área voltada à jogos.';
+let mainContent = document.getElementsByClassName('main-content');
