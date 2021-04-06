9fbc959c71732d6c284ced88d799c4bf164476f2
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-06 13:37:12 -0300

    refazendo os exercicios do dia 5.1

--- exercises/bloco5/dia1/exerciciosDoDia/exercicio_1.js
+++ exercises/bloco5/dia1/exerciciosDoDia/exercicio_1.js
@@ -1,4 +1,5 @@
 document.getElementsByTagName('p')[0].innerText = 'me vejo sendo um programador junior';
 document.getElementsByTagName('p')[1].innerText = 'trabalhando na área de backend';
 document.getElementsByTagName('p')[2].innerText = 'preferencialmente em alguma área voltada à jogos.';
-let mainContent = document.getElementsByClassName('main-content');
+let firstSquare = document.getElementsByTagName('main-content');
+firstSquare[0].style.backgroundcolor = 'red'
