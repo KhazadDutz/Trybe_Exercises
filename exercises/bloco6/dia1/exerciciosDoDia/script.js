d89fc536c08b25ef3678c5456d70c7279df40029
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-15 19:43:32 -0300

    exercicio 2 quase completo

--- exercises/bloco6/dia1/exerciciosDoDia/script.js
+++ exercises/bloco6/dia1/exerciciosDoDia/script.js
@@ -1,3 +1,16 @@
 const estadosDropDown = document.querySelector('#dropDownEstados');
 const estadosBrasil = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal*', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
-const siglasEstados = []
+
+function createSelectsOption(array) {
+  for (let estado of array) {
+    let tempEstado = document.createElement('option');
+    tempEstado.innerText = estado;
+    estadosDropDown.appendChild(tempEstado);
+  }
+}
+
+createSelectsOption(estadosBrasil);
+
+function checkDate() {
+
+}
