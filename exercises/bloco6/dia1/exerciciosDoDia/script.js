0b0c7eff350d8df6a333e6cce42ab453b742fa83
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-16 20:40:43 -0300

    reestiliza o formulario

--- exercises/bloco6/dia1/exerciciosDoDia/script.js
+++ exercises/bloco6/dia1/exerciciosDoDia/script.js
@@ -1,7 +1,7 @@
 const estadosDropDown = document.querySelector('#dropDownEstados');
 const estadosBrasil = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal*', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 const inputSubmitButton = document.querySelector('#input-submit');
-const formDocument = document.querySelector('.inputText');
+const formDocument = document.querySelectorAll('.inputText');
 const body = document.querySelector('body');
 const backupPDocs = document.querySelector('.backupDocs');
 
@@ -21,17 +21,23 @@ function preventSubmit(event) {
 
 function createDivAndP() {
   const divOutOfForm = document.createElement('div');
-  const pOutOfForm = document.createElement('p');
   pOutOfForm.className = 'backupDocs';
   body.appendChild(divOutOfForm);
-  divOutOfForm.appendChild(pOutOfForm);
 }
 
 function transferTextToP() {
   let pOutOfForm = document.querySelector('.backupDocs');
-  pOutOfForm.innerText = formDocument.value;
+  for (let doc of formDocument) {
+    let pOutOfForm = document.createElement('p');
+    pOutOfForm.innerText = formDocument.value;
+    divOutOfForm.appendChild(pOutOfForm);
+  }
 }
 
-inputSubmitButton.addEventListener('click', preventSubmit);
-inputSubmitButton.addEventListener('click', createDivAndP);
-inputSubmitButton.addEventListener('click', transferTextToP);
+function handleSubmit(event) {
+  preventSubmit(event);
+  createDivAndP();
+  transferTextToP();
+}
+
+inputSubmitButton.addEventListener('click', handleSubmit);
