ec1d21fdf27ee31ad8751b1344764effbcc8cdb0
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-27 14:52:23 -0300

    exercicio 6 quase completo

---
+++ exercises/bloco7/dia1/exerciciosDoDia/exercicio6.js
@@ -0,0 +1,15 @@
+let string = 'Tryber x aqui';
+let paramether = 'Bebeto';
+const skills = ['JavaScript', 'CSS', 'FlexBox', 'HTML', 'GitHub'];
+
+function findX(Param1, string) {
+  let splitString = string.split(' ')
+  let found = splitedString.indexOf('x');
+  splitString[found] = Param1;
+  let newString = splitedString.join(' ');
+  return newString;
+}
+
+function concatString(func, string)
+
+console.log(findX(paramether, string));
