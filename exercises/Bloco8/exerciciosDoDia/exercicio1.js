dbb6b8c4413eccf8e73e40d2f260266e4dc2c6ae
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-30 19:23:01 -0300

    exercicio 1 completo

--- exercises/Bloco8/exerciciosDoDia/exercicio1.js
+++ exercises/Bloco8/exerciciosDoDia/exercicio1.js
@@ -1,6 +1,7 @@
 const newRegistration = (nomeCompleto) => {
   let splitName = nomeCompleto.toLowerCase().split(' ');
   let stringOfNameSplit = [];
+  const employee = {};
   for (let name of splitName) {
     if (splitName.indexOf(name) < splitName.length -1) {
       stringOfNameSplit.push(`${name}_`);
@@ -8,16 +9,18 @@ const newRegistration = (nomeCompleto) => {
       stringOfNameSplit.push(`${name}@`);
     }
   }
-  return `${stringOfNameSplit.join('')}trybe.com`;
+  employee['Nome'] = nomeCompleto;
+  employee['Email'] = `${stringOfNameSplit.join('')}trybe.com`;
+  return employee;
 }
 
-const newEmployees = (func1, func2, func3) => {
+const newEmployees = (func) => {
   const employees = {
-    id1: func1,// Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
-    id2: func2,// Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
-    id3: func3,// Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
+    id1: func('Pedro Guerra'),// Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
+    id2: func('Luiza Drumond'),// Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
+    id3: func('Carla Paiva'),// Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
   }
   return employees;
 };
 
-console.log(newEmployees(newRegistration('Pedro Guerra'), newRegistration('Luiza Drumond'), newRegistration('Carla Paiva')));
+console.log(newEmployees(newRegistration));
--- exercises/Bloco8/exerciciosDoDia/exercicio2.js
+++ exercises/Bloco8/exerciciosDoDia/exercicio2.js
@@ -12,3 +12,23 @@ const raffle = (numberBet, numberDrawn) => {
 }
 
 raffle(1, numberDrawn());
+
+// --------------------------------------------------------------------
+
+const checkNumbers = (numberBet, raffleNumber) => {
+  return numberBet === raffleNumber;
+}
+
+const raffle = (numberBet, checkNumber) => {
+  min = Math.ceil(1);
+  max = Math.floor(5);
+  const raffleNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if (checkNumber(numberBet, raffleNumber)) {
+    console.log('Parabéns você ganhou');
+  } else {
+    console.log('Tente novamente');
+  }
+}
+
+raffle(3, checkNumbers);
